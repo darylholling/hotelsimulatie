306566bb18b3c5f11dd795627095dd2718f5bdc6
Mirjam <19137508@student.hhs.nl>
2020-09-19 15:33:45 +0200

    added classes

--- src/com/company/JsonReader.java
+++ src/com/company/JsonReader.java
@@ -2,7 +2,6 @@ package com.company;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.stream.JsonToken;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,7 +18,7 @@ public class JsonReader{
 
         Gson gson = new GsonBuilder().create();
 
-        String fileName = "src/layout.json";
+        String fileName = "src/com/company/layout.json";
         Path path = new File(fileName).toPath();
 
         try (Reader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
