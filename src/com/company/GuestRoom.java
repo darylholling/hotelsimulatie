8ab2b073eb68ad0cd4138976a2ea8bb6feee2f2c
Tycho <tvgrieken@gmail.com>
2020-09-21 14:33:34 +0200

    areas

--- src/com/company/GuestRoom.java
+++ src/com/company/GuestRoom.java
@@ -1,4 +1,13 @@
 package com.company;
 
-public class GuestRoom {
+public class GuestRoom extends Area{
+    private int stars;
+
+    public int getStars() {
+        return stars;
+    }
+
+    public void setStars(int stars) {
+        this.stars = stars;
+    }
 }
