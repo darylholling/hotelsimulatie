2de1581d7d99a6dc9899ddb1893d6a15ac6cd699
Mirjam <19137508@student.hhs.nl>
2020-09-22 21:18:28 +0200

    added

---
+++ src/com/company/Settings.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Settings {
+    private int hteInSeconds;
+    private int roomCleanTime;
+    private int moveFloorTime;
+
+    public Settings (int hteInSeconds, int roomCleanTime, int moveFloorTime) {
+        this.hteInSeconds = hteInSeconds;
+        this.roomCleanTime = roomCleanTime;
+        this.moveFloorTime = moveFloorTime;
+
+    }
+
+
+}
