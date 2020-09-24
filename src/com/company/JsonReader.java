faeb1ab8be6b6ca57fe22b76e6e84b8d0cb68771
Tycho <tvgrieken@gmail.com>
2020-09-24 18:39:20 +0200

    generic JsonReader added

--- src/com/company/JsonReader.java
+++ src/com/company/JsonReader.java
@@ -1,8 +1,6 @@
 package com.company;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
@@ -15,20 +13,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class JsonReader{
 
-    public static void main(String[] args) throws IOException {
-
-
+    public Layout [] readJson(String filename) throws IOException {
         Gson gson = new GsonBuilder().create();
-
-        Path path = new File("json/layout.json").toPath();
-
-        try (Reader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)) {
-
-            Layout[] layouts = gson.fromJson(reader, Layout[].class);
-            Arrays.stream(layouts).forEach(System.out::println);
-        }
-    }
-    public void readJson(){
-        
+        Path path = new File(filename).toPath();
+        Reader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8);
+        return gson.fromJson(reader, Layout[].class);
     }
 }
