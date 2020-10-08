d006e0ff736dbbf948faff1c2cadffc219e26049
Verline <38378016+vlinpalm@users.noreply.github.com>
2020-10-08 20:09:59 +0200

    Fixed dijkstra/check in event. Guests now "move" to selected rooms till no more rooms are available.

--- src/com/company/events/CheckInEvent.java
+++ src/com/company/events/CheckInEvent.java
@@ -4,6 +4,7 @@ import com.company.actions.Dijkstra;
 import com.company.actions.HotelBuilder;
 import com.company.models.Guest;
 import com.company.models.Hotel;
+import com.company.models.areas.Area;
 import com.company.models.areas.GuestRoom;
 import javafx.scene.Node;
 import javafx.scene.layout.GridPane;
@@ -28,26 +29,15 @@ public class CheckInEvent extends Event {
 
     @Override
     public void fire() {
-
         GuestRoom[] guestRooms = this.hotel.areas.stream().filter(area -> area instanceof GuestRoom).toArray(GuestRoom[]::new);
 
         GuestRoom[] availableByStars = Arrays.stream(guestRooms).filter(guestRoom -> guestRoom.getStars() == this.stars && !guestRoom.isOccupied() && !guestRoom.needsCleaning()).toArray(GuestRoom[]::new);
         // upgrade when preferred stars not available
 
-        int freeRoom = 0;
-        for (GuestRoom room : availableByStars){
-            if (!room.isOccupied()){
-                freeRoom ++;
-            }
-        }
-        if (freeRoom == 0 && this.stars <5){
+        while (availableByStars.length == 0 && this.stars <5){
             this.stars++;
             availableByStars = Arrays.stream(guestRooms).filter(guestRoom -> guestRoom.getStars() == this.stars && !guestRoom.isOccupied() && !guestRoom.needsCleaning()).toArray(GuestRoom[]::new);
         }
-        else {
-            //TODO do not check in .. do we even need a checkoutevent?
-        }
-
 
         GuestRoom selectedGuestRoom = availableByStars[new Random().nextInt(availableByStars.length)];
         System.out.println("Room Rating: "+selectedGuestRoom.getStars()+ " Location: X : "+selectedGuestRoom.getX()+" Y: " + selectedGuestRoom.getY());
@@ -61,15 +51,13 @@ public class CheckInEvent extends Event {
 
         //TODO dijkstra magic go to room with image visible.
         guest.setShown(true);
-        guest.toFront();
+        Dijkstra ds = new Dijkstra();
         guest.getArea().setDistance(0);
-        String path = this.hotel.dijkstra.findPath(guest, guest.getArea(), guest.getGuestRoom());
-        //        System.out.println(guest.getArea());
+//        String path = this.hotel.dijkstra.findPath(guest, guest.getArea(), guest.getGuestRoom());
+        String path = ds.findPath(guest, guest.getArea(), guest.getGuestRoom());
         System.out.println(path);
-//        guest.setVisible(false);
         this.hotel.guestList.add(guest);
-            System.out.println(HotelBuilder.gridPane.getChildren().getClass().getSimpleName());
-
-        //TODO go to room
+//        guest.setGuestImage();
+//    TODO go to room
     }
 }
