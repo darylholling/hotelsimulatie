bb02a190cbefe01a0ecb38823be9f529d808daa1
Tycho <tvgrieken@gmail.com>
2020-10-09 20:45:31 +0200

    Test if Timer starts and ends added

---
+++ Test/com/company/models/TimeTest.java
@@ -0,0 +1,30 @@
+package com.company.models;
+
+import javafx.application.Platform;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Timer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeTest {
+    boolean running;
+    ArrayList<HTEListener> HTElisteners;
+    Settings settings = new Settings();
+    HteCounter hteCounter;
+    Time time = new Time(HTElisteners,settings);
+
+    @Test
+    public void testIfTheTimerStartsAndStops(){
+        settings.setSettings(1,1,1,1);
+
+        Assert.assertEquals(false,time.isRunning());
+        time.startTimer();
+
+        Assert.assertEquals(true, time.isRunning());
+        time.stopTimer();
+    }
+
+}
