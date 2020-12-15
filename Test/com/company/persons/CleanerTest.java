ce9cb2c5cb5d9d8c6833a74a6b67bb8e9f7c821d
diedekerkhof <diedekerkhof@gmail.com>
2020-12-15 19:10:41 +0100

    Commit progress

--- Test/com/company/persons/CleanerTest.java
+++ Test/com/company/persons/CleanerTest.java
@@ -1,7 +1,8 @@
 package com.company.persons;
 
-import com.company.actions.EventBuilder;
 import com.company.actions.EventHandler;
+import com.company.events.CleaningEmergencyEvent;
+import com.company.events.DefaultCleaningEvent;
 import com.company.events.Event;
 import com.company.models.Hotel;
 import com.company.models.Settings;
@@ -9,13 +10,10 @@ import com.company.models.areas.GuestRoom;
 import com.company.models.areas.Lobby;
 import javafx.embed.swing.JFXPanel;
 import org.junit.Assert;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayDeque;
 import java.util.Objects;
 import java.util.Queue;
 
@@ -24,36 +22,69 @@ public class CleanerTest {
     private static EventHandler eventHandle = new EventHandler(hotel);
     private static Queue<Event> eventQueue;
 
-
     @BeforeAll
-    public static void prepareForTest() throws IOException {
+    public static void prepare() throws IOException {
         JFXPanel jfxPanel = new JFXPanel();
         Settings.getSettings().setEventsFile(new File("Test/jsonTestFiles/CleanerEventsTest.json"));
         eventHandle.handleStart();
+        eventQueue = eventHandle.getEventsQueue();
         Lobby lobby = new Lobby(0, 0, 0, 0);
         hotel.areas.add(lobby);
         GuestRoom guestRoom = new GuestRoom(0, 0, 1, 0, 1);
         guestRoom.setHotel(hotel);
         hotel.areas.add(guestRoom);
+        triggerNextNormalEvent();
+    }
+
+    @BeforeEach
+    public void prepareForTest() {
         hotel.createCleaners();
-        eventQueue = eventHandle.getEventsQueue();
-        System.out.println(eventHandle.getEventsQueue());
-        triggerNextEvent();
-        triggerNextEvent();
+    }
+
+    @AfterEach
+    public void reset() {
+        hotel.cleaners.clear();
     }
 
     @Test
     public void checkIfCleanerPicksUpCleaningEvent() {
-        System.out.println(hotel.cleaningEmergencyEvents);
-        Objects.requireNonNull(hotel.cleaningEmergencyEvents.poll()).fire();
-        System.out.println(hotel.guestList);
-        hotel.cleaners.get(0).startCleaners();
-        System.out.println(hotel.cleaners.get(0).getCurrentCleanEvent());
+        triggerNextEmergencyCleaningEvent();
+        startCleaner();
 
         Assert.assertNotNull(hotel.cleaners.get(0).getCurrentCleanEvent());
     }
 
-    private static void triggerNextEvent() {
+    @Test
+    public void checkIfCleanerPicksUpDefaultCleaningEvent() {
+        triggerNextDefaultCleaningEvent();
+        startCleaner();
+
+        Assert.assertTrue(hotel.cleaners.get(0).getCurrentCleanEvent() instanceof DefaultCleaningEvent);
+    }
+
+    @Test
+    public void checkIfCleanerPicksUpEmergencyCleaningEvent() {
+        triggerNextEmergencyCleaningEvent();
+        startCleaner();
+
+        Assert.assertTrue((hotel.cleaners.get(0).getCurrentCleanEvent() instanceof CleaningEmergencyEvent));
+    }
+
+    private static void triggerNextNormalEvent() {
         Objects.requireNonNull(eventQueue.poll()).fire();
+    }
+
+    private static void triggerNextEmergencyCleaningEvent() {
+        triggerNextNormalEvent();
+        Objects.requireNonNull(hotel.cleaningEmergencyEvents.poll()).fire();
+    }
+
+    private static void triggerNextDefaultCleaningEvent() {
+        triggerNextNormalEvent();
+        Objects.requireNonNull(hotel.defaultCleaningEvents.poll()).fire();
+    }
+
+    private void startCleaner() {
+        hotel.cleaners.get(0).startCleaners();
     }
 }
